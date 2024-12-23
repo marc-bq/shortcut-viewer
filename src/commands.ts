09bcf24ef7bf88bd137ddc5b2e98b16b20e46650
Marc Baldevey <marcbaldevey@inedit.com>
2024-12-23 17:22:31 +0100

    Enhance commitStory command to update story state and add comments in Shortcut

--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,13 @@
-import { StorySlim } from '@shortcut/client';
+import ShortcutClient, { StorySlim } from '@shortcut/client';
 import { exec } from 'child_process';
 import * as vscode from 'vscode';
 
 export const commitStory = (task:any) => {
     const commitMessage = task.label;
     const options = { cwd: vscode.workspace.workspaceFolders? vscode.workspace.workspaceFolders[0].uri.path:undefined };
+    const packageJson = require(`${options.cwd}/package.json`);
+    const token:string = vscode.workspace.getConfiguration('shortcutViewer').get('token')||'';
+    const closedState:number|undefined = vscode.workspace.getConfiguration('shortcutViewer').get('closedStage');
     exec('git add .',options, (addError, addStdout, addStderr) => {
         if (addError) {
             vscode.window.showErrorMessage(`Git add failed: ${addStderr}`);
@@ -19,6 +22,19 @@ export const commitStory = (task:any) => {
             vscode.window.showInformationMessage(`Committed: ${commitMessage}`);
             // Store the commit URL in a variable or use it as needed
             console.log('Commit URL:', commitUrl);
+            if(closedState && token) {
+                const shortcurClient = new ShortcutClient(token);
+                shortcurClient.updateStory(task.shortcutTask.id, {workflow_state_id: closedState});
+                const version = packageJson.version;
+                const name = packageJson.name;
+                shortcurClient.createStoryComment(task.shortcutTask.id, {text: `Committed: ${commitUrl}`});
+                shortcurClient.updateStory(task.shortcutTask.id, {labels: [{name: `${name}-${version}`,color: '#FC5000'}]});
+                // shortcurClient.listLabels({slim:true}).then((result) => {
+                //     const label = result.data.find((label:any) => label.name === `${name}-${version}`);
+                //     if(!label) {
+                //     }
+                // }
+            }
         });
     });
 }
